053366b19be5597a0854c937cacaf4a49ac80418
k1o0 <k1o0@3tk.co>
2017-08-17 13:55:17 +0100

    Added a few comments to listeners section

--- +exp/SignalsExp.m
+++ +exp/SignalsExp.m
@@ -204,16 +204,16 @@ classdef SignalsExp < handle
         obj.LickDetector = rig.lickDetector;
       end
       if ~isempty(obj.DaqController.SignalGenerators)
-          outputNames = fieldnames(obj.Outputs);
+          outputNames = fieldnames(obj.Outputs); % Get list of all outputs specified in expDef function
           for m = 1:length(outputNames)
               id = find(strcmp(outputNames{m},...
-                  obj.DaqController.ChannelNames));
-              if id
+                  obj.DaqController.ChannelNames)); % Find matching channel from rig hardware file
+              if id % if the output is present, create callback 
                   obj.Listeners = [obj.Listeners
-                    obj.Outputs.(outputNames{m}).onValue(@(v)obj.DaqController.command([zeros(1,id-1) v]))
+                    obj.Outputs.(outputNames{m}).onValue(@(v)obj.DaqController.command([zeros(1,id-1) v])) % pad value with zeros in order to output to correct channel
                     obj.Outputs.(outputNames{m}).onValue(@(v)fprintf('delivering output of %.2f\n',v))
                     ];   
-              elseif strcmp(outputNames{m}, 'reward')
+              elseif strcmp(outputNames{m}, 'reward') % special case; rewardValve is always first signals generator in list 
                   obj.Listeners = [obj.Listeners
                     obj.Outputs.reward.onValue(@(v)obj.DaqController.command(v))
                     obj.Outputs.reward.onValue(@(v)fprintf('delivering reward of %.2f\n', v))
