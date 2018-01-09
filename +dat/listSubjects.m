6e274b15029924cab9e64b16800d074ab51ae796
nsteinme <nick.steinmetz@gmail.com>
2018-01-09 12:20:39 +0000

    move whichExpNums to cortexlab, update dat.listSubjects and .subjectSelector, add docs

--- +dat/listSubjects.m
+++ +dat/listSubjects.m
@@ -1,17 +1,43 @@
-function subjects = listSubjects()
+function subjects = listSubjects(varargin)
 %DAT.LISTSUBJECTS Lists recorded subjects
-%   subjects = DAT.LISTSUBJECTS() Lists the experimental subjects present
+%   subjects = DAT.LISTSUBJECTS([alyxInstance]) Lists the experimental subjects present
 %   in experiment info repository ('expInfo').
+%
+% Optional input argument of an alyx instance will enable generating this
+% list from alyx rather than from the directory structure on zserver
 %
 % Part of Rigbox
 
 % 2013-03 CB created
+% 2018-01 NS added alyx compatibility
 
-% The master 'expInfo' repository is the reference for the existence of
-% experiments, as given by the folder structure
-expInfoPath = dat.reposPath('expInfo', 'master');
-
-dirs = file.list(expInfoPath, 'dirs');
-subjects = setdiff(dirs, {'misc'}); %exclude the misc directory
-
+if nargin>0 && ~isempty(varargin{1}) % user provided an alyx instance
+    ai = varargin{1}; % an alyx instance
+    
+    % get list of all living, non-stock mice from alyx
+    s = alyx.getData(ai, 'subjects?stock=False&alive=True');
+    
+    % determine the user for each mouse
+    respUser = cellfun(@(x)x.responsible_user, s, 'uni', false);
+    
+    % get cell array of subject names
+    subjNames = cellfun(@(x)x.nickname, s, 'uni', false);
+    
+    % determine which subjects belong to this user
+    thisUserSubs = sort(subjNames(strcmp(respUser, ai.username)));
+    
+    % all the subjects
+    otherUserSubs = sort(subjNames(~strcmp(respUser, ai.username)));
+    
+    % the full, ordered list
+    subjects = [{'default'}, thisUserSubs, otherUserSubs]';
+else
+    
+    % The master 'expInfo' repository is the reference for the existence of
+    % experiments, as given by the folder structure
+    expInfoPath = dat.reposPath('expInfo', 'master');
+    
+    dirs = file.list(expInfoPath, 'dirs');
+    subjects = setdiff(dirs, {'misc'}); %exclude the misc directory
+end
 end
--- cortexlab/+dat/whichExpNums.m
+++ cortexlab/+dat/whichExpNums.m
@@ -1,6 +1,22 @@
 function [expNums, blocks, hasBlock, pars, isMpep, tl, hasTimeline] = ...
     whichExpNums(mouseName, thisDate)
-
+% [expNums, blocks, hasBlock, pars, isMpep, tl, hasTimeline] = ...
+%     whichExpNums(mouseName, thisDate)
+%
+% Attempt to automatically determine what experiments of what types were
+% run for a subject on a given date. 
+%
+% Returns:
+% - expNums - list of the experiment numbers that exist
+% - blocks - cell array of the Block structs
+% - hasBlock - boolean array indicating whether each experiment had a block
+% - pars - cell array of parameters structs
+% - isMpep - boolean array of whether the experiment was mpep type
+% - tl - cell array of Timeline structs
+% - hasTimeline - boolean array of whether timeline was present for each
+% experiment
+%
+% Created by NS 2017
 
 rootExp = dat.expFilePath(mouseName, thisDate, 1, 'Timeline', 'master');
 expInf = fileparts(fileparts(rootExp));
@@ -26,6 +42,8 @@ for e = 1:length(expNums)
         hasBlock(e) = true;
     end
 
+    % if there is a parameters file, load it and determine whether it is
+    % mpep type
     dPars = dat.expFilePath(mouseName, thisDate, expNums(e), 'parameters', 'master');
     if exist(dPars)
         load(dPars)
@@ -37,8 +55,7 @@ for e = 1:length(expNums)
     end
         
 
-    % if there is a timeline, load it and get photodiode events, mpep UDP
-    % events.
+    % if there is a timeline, load it 
     dTL = dat.expFilePath(mouseName, thisDate, expNums(e), 'Timeline', 'master');
     if exist(dTL)
         fprintf(1, 'expNum %d has timeline\n', e);        
