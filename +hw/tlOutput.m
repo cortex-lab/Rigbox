0a00f43d11c80c0119deb25b7a8d33263e085741
nsteinme <nick.steinmetz@gmail.com>
2018-01-25 18:56:40 +0000

    first commit with working chrono and acqLive classes

---
+++ +hw/tlOutput.m
@@ -0,0 +1,36 @@
+classdef tlOutput < matlab.mixin.Heterogeneous & handle
+  %hw.tlOutput Code to specify an output channel for timeline
+  %   This is an abstract class. 
+  %
+  %   Below is a list of some subclasses and their functions:
+  %     hw.tlOutputClock - clocked output on a counter channel
+  %     hw.tlOutputChrono - the default, flip/flip status check output
+  %     hw.tlOutputAcqLive - a digital channel that is on for the duration
+  %     of the recording
+  %     hw.tlOutputStartStopSync - a digital channel that turns on only at
+  %     the beginning and end of the recording
+  %
+  %   The timeline object will call the onLoad, onStart, and onStop
+  %   methods.
+  %
+  % Part of Rigbox
+  
+  % 2018-01 NS created
+  
+  properties
+    name
+    session
+  end
+  
+  methods (Abstract)
+    onInit(obj, timeline)
+    onStart(obj, timeline)
+    onProcess(obj, timeline, event)
+    onStop(obj, timeline)
+    %s = propertiesAsStruct(obj) % recommend we have a method that does this, 
+    %   so that we can save out all the properties in a json file. Incl a
+    %   version number?
+  end
+  
+end
+
---
+++ +hw/tlOutputAcqLive.m
@@ -0,0 +1,52 @@
+classdef tlOutputAcqLive < hw.tlOutput
+  %hw.tlOutputAcqLive A digital signal that goes up when the recording starts, 
+  %     down when it ends.
+  % See also hw.tlOutput and hw.Timeline
+  %
+  % Part of Rigbox
+  % 2018-01 NS
+  
+  properties
+    daqDeviceID
+    daqChannelID
+    daqVendor = 'ni'
+    initialDelay = 0
+  end
+  
+  methods
+    function obj = tlOutputAcqLive(name, daqDeviceID, daqChannelID)
+      obj.name = name;
+      obj.daqDeviceID = daqDeviceID;
+      obj.daqChannelID = daqChannelID;      
+    end
+
+    function onInit(obj, ~)
+        fprintf(1, 'initialize acqLive\n');
+        obj.session = daq.createSession(obj.daqVendor);
+        obj.session.addDigitalChannel(obj.daqDeviceID, obj.daqChannelID, 'OutputOnly');
+        outputSingleScan(obj.session, false);
+    end
+    
+    function onStart(obj, ~)     
+        fprintf(1, 'start acqLive\n');
+        
+        pause(obj.initialDelay);
+        outputSingleScan(obj.session, true);
+        
+    end
+    
+    function onProcess(~, ~, ~)
+        fprintf(1, 'process acqLive\n');
+        
+    end
+    
+    function onStop(obj,~)
+        fprintf(1, 'stop chrono\n');
+        stop(obj.session);
+        release(obj.session);
+    end
+    
+  end
+  
+end
+
