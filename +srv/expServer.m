8975458df92f1b1252ef8c4125aae3bd4d425e99
k1o0 <k1o0@3tk.co>
2018-05-10 11:54:03 +0100

    HideCurser now back

--- +srv/expServer.m
+++ +srv/expServer.m
@@ -55,7 +55,7 @@ cleanup = onCleanup(@() fun.applyForce({
   @() PsychPortAudio('Verbosity', oldPpaVerbosity)...
   }));
 
-% HideCursor();
+HideCursor();
 
 if nargin < 2
   bgColour = 127*[1 1 1]; % mid gray by default
