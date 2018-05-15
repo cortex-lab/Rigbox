192d34a5633dba1de5e692ea85eb5ad3f91342bb
k1o0 <k1o0@3tk.co>
2018-05-15 11:54:26 +0100

    Added Nick's sexy exp panel (may be too intensive)

---
+++ Repositories/code/Rigging/ExpDefinitions/screenImage.m
@@ -0,0 +1,47 @@
+
+
+function [x,y,im] = screenImage(pars)
+
+cL = pars.stimulusContrast(1); 
+cR = pars.stimulusContrast(2); 
+
+sf = pars.spatialFrequency;
+ori = pars.stimulusOrientation;
+% al = pars.stimulusAltitude;
+az = pars.stimulusAzimuth;
+
+sigma = 10;
+
+pixPerDeg = 3;
+
+bgc = 127;
+
+xExtent = 540;
+im = ones(70*pixPerDeg,xExtent*pixPerDeg)*bgc;
+x = linspace(-xExtent/2, xExtent/2, size(im,2));
+y = linspace(-35, 35, size(im,1));
+
+gratSize = sigma*7*pixPerDeg;
+gw = (gausswin(gratSize, 1/(sigma*pixPerDeg/gratSize*2))*gausswin(gratSize, 1/(sigma*pixPerDeg/gratSize*2))');
+gw = gw./max(gw(:));
+
+% sine wave
+gratL = imrotate(repmat(sin([1:gratSize]/gratSize*2*pi*gratSize/pixPerDeg*sf),gratSize,1).*gw*cL,ori,'bilinear','crop');
+gratR = imrotate(repmat(sin([1:gratSize]/gratSize*2*pi*gratSize/pixPerDeg*sf),gratSize,1).*gw*cR,ori,'bilinear','crop');
+
+gratL = gratL*127+bgc;
+gratR = gratR*127+bgc;
+
+sy = round(size(im,1)/2);
+insertIndsY = (1:gratSize)+sy-round(gratSize/2);
+
+sx = round((-az+xExtent/2)*pixPerDeg);
+insertInds = (1:gratSize)+sx-round(gratSize/2);
+incl = insertInds>0&insertInds<size(im,2);
+im(insertIndsY, insertInds(incl)) = gratL(:,incl);
+
+sx = round((az+xExtent/2)*pixPerDeg);
+insertInds = (1:gratSize)+sx-round(gratSize/2);
+incl = insertInds>0&insertInds<size(im,2);
+im(insertIndsY, insertInds(incl)) = gratR(:,incl);
+
