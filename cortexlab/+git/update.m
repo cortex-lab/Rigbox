00226a713ec77529fd7e1406cc64f1f62de34769
k1o0 <k1o0@3tk.co>
2018-10-18 22:47:52 +0100

    mc & expServer pull latest code upon starting

---
+++ cortexlab/+git/update.m
@@ -0,0 +1,37 @@
+function update(essential)
+% UPDATE Update Rigbox code
+%  TODO
+%
+if nargin == 0; essential = true; end
+gitexepath = getOr(dat.paths, 'gitExe', 'C:\Program Files\Git\cmd\git.exe'); %TODO generalize
+gitexepath = ['"' gitexepath '"'];
+root = fileparts(which('addRigboxPaths'));
+origDir = pwd;
+cd(root)
+
+cmdstr = strjoin({gitexepath, 'pull'});
+[status, cmdout] = system(cmdstr);
+if status ~= 0
+  if essential
+    cd(origDir)
+    error('gitUpdate:pull:pullFailed', 'Failed to pull latest changes:, %s', cmdout)
+  else
+    warning('gitUpdate:pull:pullFailed', 'Failed to pull latest changes:, %s', cmdout)
+  end
+end
+% TODO: check if submodules are empty and use init flag
+cmdstr = strjoin({gitexepath, 'submodule update --remote --merge'});
+status = system(cmdstr);
+if status ~= 0
+  if essential
+    cd(origDir)
+    error('gitUpdate:submodule:updateFailed', ...
+      'Failed to pull latest changes for submodules:, %s', cmdout)
+  else
+    warning('gitUpdate:submodule:updateFailed', ...
+      'Failed to pull latest changes for submodules:, %s', cmdout)
+  end
+end
+
+cd(origDir)
+end
