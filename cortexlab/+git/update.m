82095ae6295d2d64321121dcc0065d828c80e208
Jai Bhagat <j.bhagat@ucl.ac.uk>
2019-01-28 16:22:31 +0000

    additional comments for git.update

--- cortexlab/+git/update.m
+++ cortexlab/+git/update.m
@@ -1,32 +1,34 @@
 function update(scheduled)
 % GIT.UPDATE Pull latest Rigbox code 
-%   Pulls the latest code from the remote repository.  If scheduled is a
-%   value in the range [1 7] corresponding to the days of the week starting
-%   Sunday, the function will only continue on that day, provided the last
-%   fetch was over a day ago.  If it is not the scheduled day, but the last
-%   fetch was over a week ago, the function will pull changes.  If
-%   scheduled is false, the function will pull changes provided the last
-%   fetch was over an hour ago.
+%   Pulls the latest code from the remote Github repository. If 'scheduled' 
+%   is a value in the range [1 7] - corresponding to the days of the week, 
+%   with Sunday=1 - code will be pulled only on the 'scheduled' day, 
+%   provided the last fetch was over a day ago. Code will also be pulled if 
+%   it is not the scheduled day, but the last fetch was over a week ago. If
+%   scheduled is 0, the function will pull changes provided the last fetch
+%   was over an hour ago.
 %
 % TODO Find quicker way to check for changes
 % See also DAT.PATHS
-if nargin < 1; scheduled = getOr(dat.paths, 'updateSchedule', 0); end
 
-root = fileparts(which('addRigboxPaths'));
+% If not given as input argument, find 'scheduled' in 'dat.paths'. If not
+% found, set 'scheduled' to 0.
+if nargin < 1; scheduled = getOr(dat.paths, 'updateSchedule', 0); end
+root = fileparts(which('addRigboxPaths')); % Rigbox root directory
 % Attempt to find date of last fetch
 fetch_head = fullfile(root, '.git', 'FETCH_HEAD');
 lastFetch = iff(exist(fetch_head,'file')==2, ... % If FETCH_HEAD file exists
   @()getOr(dir(fetch_head), 'datenum'), 0); % Retrieve date modified
 
 % Don't pull changes if the following conditions are met:
-% 1. The updates are scheduled for a different day and the last fetch was less
-% than a week ago.
+% 1. The updates are scheduled for a different day and the last fetch was
+% less than a week ago.
 % 2. The updates are scheduled for today and the last fetch was today.
 % 3. The updates are scheduled for every day and the last fetch was less
 % than an hour ago.
-if (scheduled && weekday(now) ~= scheduled && now - lastFetch < 7) || ...
-    (scheduled && weekday(now) == scheduled && now - lastFetch < 1) || ...
-        (~scheduled && now - lastFetch < 1/24)
+if ((scheduled && weekday(now)) ~= (scheduled && (now - lastFetch < 7))) || ...
+    ((scheduled && weekday(now)) == (scheduled && (now - lastFetch < 1))) || ...
+    (~scheduled && (now - lastFetch < 1/24))
   return
 end
 disp('Updating code...')
@@ -34,7 +36,7 @@ disp('Updating code...')
 % Get the path to the Git exe
 gitexepath = getOr(dat.paths, 'gitExe');
 if isempty(gitexepath)
-  [~,gitexepath] = system('where git'); % this doesn't always work
+  [~,gitexepath] = system('where git'); % todo: this doesn't always work
 end
 gitexepath = ['"', strtrim(gitexepath), '"'];
 
@@ -42,6 +44,8 @@ gitexepath = ['"', strtrim(gitexepath), '"'];
 origDir = pwd;
 cd(root)
 
+% Create Windows system commands for git stashing, initializing submodules,
+% and pulling
 cmdstrStash = [gitexepath, ' stash push -m "stash Rigbox working changes before scheduled git update"'];
 cmdstrStashSubs = [gitexepath, ' submodule foreach "git stash push"'];
 cmdstrInit = [gitexepath, ' submodule update --init'];
