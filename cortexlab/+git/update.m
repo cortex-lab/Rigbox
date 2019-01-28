3d90407b2353337a7e5d790537a0fc791a9abbf5
k1o0 <k1o0@3tk.co>
2019-01-28 17:43:37 +0200

    Update only occurs once on scheduled day

--- cortexlab/+git/update.m
+++ cortexlab/+git/update.m
@@ -1,11 +1,15 @@
 function update(scheduled)
 % GIT.UPDATE Pull latest Rigbox code 
 %   Pulls the latest code from the remote repository.  If scheduled is a
-%   value in the range [1 7] corresponding to the days of the week, the
-%   function will only continue on that day, or if the last fetch was over
-%   a week ago.  
+%   value in the range [1 7] corresponding to the days of the week starting
+%   Sunday, the function will only continue on that day, provided the last
+%   fetch was over a day ago.  If it is not the scheduled day, but the last
+%   fetch was over a week ago, the function will pull changes.  If
+%   scheduled is false, the function will pull changes provided the last
+%   fetch was over an hour ago.
+%
 % TODO Find quicker way to check for changes
-% See also
+% See also DAT.PATHS
 if nargin < 1; scheduled = getOr(dat.paths, 'updateSchedule', 0); end
 
 root = fileparts(which('addRigboxPaths'));
@@ -13,9 +17,15 @@ root = fileparts(which('addRigboxPaths'));
 fetch_head = fullfile(root, '.git', 'FETCH_HEAD');
 lastFetch = iff(exist(fetch_head,'file')==2, ... % If FETCH_HEAD file exists
   @()getOr(dir(fetch_head), 'datenum'), 0); % Retrieve date modified
-% If the code has not been fetched in over a week, force and update,
-% otherwise return
+
+% Don't pull changes if the following conditions are met:
+% 1. The updates are scheduled for a different day and the last fetch was less
+% than a week ago.
+% 2. The updates are scheduled for today and the last fetch was today.
+% 3. The updates are scheduled for every day and the last fetch was less
+% than an hour ago.
 if (scheduled && weekday(now) ~= scheduled && now - lastFetch < 7) || ...
+    (scheduled && weekday(now) == scheduled && now - lastFetch < 1) || ...
         (~scheduled && now - lastFetch < 1/24)
   return
 end
@@ -39,10 +49,10 @@ cmdstrPull = [gitexepath, ' pull --recurse-submodules --strategy-option=theirs']
 
 % Stash any WIP, check submodules are initialized, pull
 try
-  [status, cmdout] = system(cmdstrStash, '-echo');
-  [status, cmdout] = system(cmdstrStashSubs, '-echo');
-  [status, cmdout] = system(cmdstrInit, '-echo');
-  [status, cmdout] = system(cmdstrPull, '-echo');
+  [~, cmdout] = system(cmdstrStash, '-echo');
+  [~, cmdout] = system(cmdstrStashSubs, '-echo');
+  [~, cmdout] = system(cmdstrInit, '-echo');
+  [~, cmdout] = system(cmdstrPull, '-echo'); %#ok<ASGLU>
 catch ex
   cd(origDir)
   error('gitUpdate:pull:pullFailed', 'Failed to pull latest changes:, %s', cmdout)
