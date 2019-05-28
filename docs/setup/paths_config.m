aef34697f4fcd2e03d74f1cf38c35cf00cf205eb
jaib1 <j.bhagat@ucl.ac.uk>
2019-05-28 19:40:15 +0100

    started 'paths_config' and updated 'readme'

---
+++ docs/setup/paths_config.m
@@ -0,0 +1,18 @@
+% The 'dat.paths' file is used for configuring important paths for the
+% computers which Rigbox runs on and communicates with. These include paths
+% to: 
+%
+% 1) either the shared folder(s) OR the remote server(s) on which 
+% organization-wide configuration files, subject data and experiment data 
+% is stored, and a local directory for generating redundant copies of this 
+% data 
+% 
+% 2) optionally, paths to a remote database (e.g., if using Alyx), 
+% and a local redundant copy of that database
+%
+% 3) optionally, paths to any other directories for storing additional
+% back-ups (e.g. for working analyses, tapes, etc...)
+%
+% 4) optionally, a path to a custom config file for the local computer.
+%
+% @todo: add instructions w/ code examples for setting up 'dat.paths' 
