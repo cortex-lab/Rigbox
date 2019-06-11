39efa70aa3bb73691d041c7cfa373c7acedf2c8e
k1o0 <k1o0@3tk.co>
2019-06-11 17:02:11 +0300

    Moved activation default from MControl to AlyxPanel

--- +eui/MControl.m
+++ +eui/MControl.m
@@ -763,12 +763,7 @@ classdef MControl < handle
       leftSideBox.Heights = [55 22];
       
       % Create the Alyx panel
-      
-      % check to see if there is a remote database url defined in
-      % `dat.paths`
-      url = char(getOr(dat.paths, 'databaseURL', ''));
-      % if so, activate AlyxPanel, if not, disable AlyxPanel
-      obj.AlyxPanel = eui.AlyxPanel(headerBox, ~isempty(url));
+      obj.AlyxPanel = eui.AlyxPanel(headerBox);
       addlistener(obj.NewExpSubject, 'SelectionChanged', @(src, evt)obj.AlyxPanel.dispWaterReq(src, evt));
       addlistener(obj.LogSubject, 'SelectionChanged', @(src, evt)obj.AlyxPanel.dispWaterReq(src, evt));
       
