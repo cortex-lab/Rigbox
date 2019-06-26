d5d030252996c96a893ef12bb73ec33a8b383e98
k1o0 <k1o0@3tk.co>
2019-06-26 15:21:40 +0300

    setValuesTest (#162) squash and rebase onto dev
    * Started setVals test for param editor
    
    * Finished ParamEditor tests; changed Timeline flags to switches

--- tests/ParamEditor_test.m
+++ tests/ParamEditor_test.m
@@ -1,5 +1,6 @@
-classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture(...
-[fileparts(mfilename('fullpath')) '\fixtures'])})... % add 'fixtures' folder as test fixture 
+classdef (SharedTestFixtures={ % add 'fixtures' folder as test fixture
+    matlab.unittest.fixtures.PathFixture('fixtures'),...
+    matlab.unittest.fixtures.PathFixture(['fixtures' filesep 'util'])})... 
   ParamEditor_test < matlab.unittest.TestCase
   
   properties
@@ -259,11 +260,45 @@ classdef (SharedTestFixtures={matlab.unittest.fixtures.PathFixture(...
         [PE.Parameters.numTrialConditions, numel(PE.Parameters.TrialSpecificNames)])
     end
     
-    function test_setValues(testCase)
-      % TODO Add test for the set values button.  For now let's fail this
+    function test_setSelectedValues(testCase)
+      %   (1:10:100) % Sets selected rows to [1 11 21 31 41 51 61 71 81 91]
+      %   @(~)randi(100) % Assigned random integer to each selected row
+      %   @(a)a*50 % Multiplies each condition value by 50
+      %   false % Sets all selected rows to false
       testCase.assertTrue(~testCase.Changed, 'Changed flag incorrect')
-%       PE = testCase.ParamEditor;
-      testCase.assertTrue(false, 'Test not implemented')
+      mock = MockDialog.instance('char');
+      mock.InTest = true;
+      mock.UseDefaults = false;
+      mock.Dialogs('Set values') = fun.CellSeq.create({'(1:10:100)', '@(~)randi(100)', '@(a)a*50'});
+
+      % Select some cells to set
+      event.Indices = [(1:5)' ones(5,1)*4];
+      selection_fn = testCase.Table.CellSelectionCallback;
+      selection_fn([],event)
+
+      % Retrieve function handle for set condition
+      callback_fn = pick(findobj(testCase.Figure,...
+        'String', 'Set values'), 'Callback');
+      callback_fn()
+
+      % Verify Changed event triggered
+      testCase.verifyTrue(testCase.Changed, ...
+        'Failed to notify listeners of parameter change')
+      
+      P = testCase.ParamEditor.Parameters;
+      % Verify values changed
+      expected = (1:10:100);
+      testCase.verifyEqual(P.Struct.numRepeats(1:5), expected(1:5), ...
+        'Failed to modify parameters')
+      
+      callback_fn()
+      values = P.Struct.numRepeats(1:5);
+      testCase.verifyTrue(all(values < 100), ...
+        'Failed to modify parameters')
+      
+      callback_fn()
+      testCase.verifyEqual(P.Struct.numRepeats(1:5)/50, values, ...
+        'Failed to modify parameters')
     end
     
     function test_sortByColumn(testCase)
