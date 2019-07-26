c13a91e2b019ad672bfdaaab6026391c63021dcc
jaib1 <j.bhagat@ucl.ac.uk>
2019-07-26 13:52:38 +0100

    fixed up git.update + doc, have to test

--- cortexlab/+git/update.m
+++ cortexlab/+git/update.m
@@ -1,24 +1,53 @@
 function update(scheduled)
-% GIT.UPDATE Pull latest Rigbox code 
-%   Pulls the latest code from the remote Github repository. If 'scheduled' 
-%   is a value in the range [1 7] - corresponding to the days of the week, 
-%   with Sunday=1 - code will be pulled only on the 'scheduled' day, 
-%   provided the last fetch was over a day ago. Code will also be pulled if 
-%   it is not the scheduled day, but the last fetch was over a week ago. If
-%   scheduled is 0, the function will pull changes provided the last fetch
-%   was over an hour ago.
+% GIT.UPDATE Pulls latest Rigbox code 
+%   `git.update` pulls the latest code from the remote Rigbox Github 
+%   repository if it is run on a specific day of the week (provided the 
+%   remote code was last fetched over a day ago), or immediately (provided 
+%   the remote code was last fetched over an hour ago), according to 
+%   `scheduled`. If run without an input arg, code is pulled according to 
+%   the `updateSchedule` field in the struct returned by `dat.paths`, or 
+%   immediately if the `updateSchedule` field is not found in `dat.paths`
+%   (provided the remote code was last fetched over an hour ago).
+%   
+%   Inputs:
+%     `scheduled`: an optional input as an integer in the interval [0,7]. 
+%     When 0, the remote code will be pulled provided the last fetch was 
+%     over an hour ago. When in the interval [1,7], code will be pulled on 
+%     a corresponding day according to `weekday` (Sunday=1, Monday=2, ... 
+%     Saturday = 7), provided the last fetch was over a day ago. Code will 
+%     also be pulled if it is not the scheduled day, but the last fetch was 
+%     over a week ago. If scheduled is 0, the function will pull changes 
+%     provided the last fetch was over an hour ago.
+%
+%   Example: Pull remote code immediately, provided last code fetch was 
+%   over an hour ago:
+%     git.update(0);
+%
+%   Example: Pull remote code if today is Monday, provided last code fetch
+%   was over a day ago:
+%     git.update(2);
+% 
+% See also DAT.PATHS
 %
 % TODO Find quicker way to check for changes
-% See also DAT.PATHS
 
-% If not given as input argument, use 'updateSchedule' in 'dat.paths'. If
-% not found, set 'scheduled' to 0.
-if nargin < 1; scheduled = getOr(dat.paths, 'updateSchedule', 0); end
+% when this function terminates, return to the working directory
+origDir = pwd;
+cleanup = onCleanup(@() cd(origDir));
+
+% If no input arg, or input arg is not an acceptable value, use 
+% `updateSchedule` in `dat.paths`. If `updateSchedule` is not found, set 
+% `scheduled` to 0.
+if nargin < 1 || ~any(scheduled == [0:7])
+  scheduled = getOr(dat.paths, 'updateSchedule', 0);
+end
 root = fileparts(which('addRigboxPaths')); % Rigbox root directory
 % Attempt to find date of last fetch
 fetch_head = fullfile(root, '.git', 'FETCH_HEAD');
-lastFetch = iff(exist(fetch_head,'file')==2, ... % If FETCH_HEAD file exists
-  @()getOr(dir(fetch_head), 'datenum'), 0); % Retrieve date modified
+% If FETCH_HEAD file exists, retrieve datenum when modified, else return 0
+% (i.e. there was never a fetch) 
+lastFetch = iff(exist(fetch_head,'file')==2, ... 
+  @()getOr(dir(fetch_head), 'datenum'), 0); 
 
 % Don't pull changes if the following conditions are met:
 % 1. The updates are scheduled for a different day and the last fetch was
@@ -26,24 +55,32 @@ lastFetch = iff(exist(fetch_head,'file')==2, ... % If FETCH_HEAD file exists
 % 2. The updates are scheduled for today and the last fetch was today.
 % 3. The updates are scheduled for every day and the last fetch was less
 % than an hour ago.
-if (scheduled && (weekday(now) ~= scheduled) && now - lastFetch < 7) || ...
-    (scheduled && (weekday(now) == scheduled) && now - lastFetch < 1) || ...
-        (~scheduled && now - lastFetch < 1/24)
+notFetchDay = scheduled ~= weekday(now) && now-lastFetch < 7;
+fetchDayButAlreadyFetched = scheduled == weekday(now) && now-lastFetch < 1;
+fetchEverydayButAlreadyFetched = scheduled == 0 && now-lastFetch < 1/24;
+if notFetchDay... 
+   || fetchDayButAlreadyFetched... 
+   || fetchEverydayButAlreadyFetched
   return
 end
 disp('Updating code...')
 
-% Get the path to the Git exe
+% Search `dat.paths` for the path to the Git exe; if not found, perform a
+% system search; if still not found, throw an error.
 gitexepath = getOr(dat.paths, 'gitExe');
 if isempty(gitexepath)
-  [~,gitexepath] = system('where git'); % todo: this doesn't always work
+  [exitCode, gitexepath] = system('where git');
+  if exitCode
+    error(['Could not find the git executable on your system. Please '
+           'ensure that you have git installed, and assign it''s full path'...
+           '(as a char array) to `p.gitExe` in your `+dat/paths.m` file.']); 
+  end
 end
+
+% Convert to string for running system commands.
 gitexepath = ['"', strtrim(gitexepath), '"'];
-
-% Temporarily change directory into Rigbox to git pull
-origDir = pwd;
+% Temporarily change directory into Rigbox to run git pull.
 cd(root)
-
 % Create Windows system commands for git stashing, initializing submodules,
 % and pulling
 cmdstrStash = [gitexepath, ' stash push -m "stash Rigbox working changes before scheduled git update"'];
@@ -52,21 +89,13 @@ cmdstrInit = [gitexepath, ' submodule update --init'];
 cmdstrPull = [gitexepath, ' pull --recurse-submodules --strategy-option=theirs'];
 
 % Stash any WIP, check submodules are initialized, pull
-try
-  [~, cmdout] = system(cmdstrStash, '-echo');
-  [~, cmdout] = system(cmdstrStashSubs, '-echo');
-  [~, cmdout] = system(cmdstrInit, '-echo');
-  [~, cmdout] = system(cmdstrPull, '-echo'); %#ok<ASGLU>
-catch ex
-  cd(origDir)
+[exitCode(1), cmdout] = system(cmdstrStash, '-echo'); %#ok<*ASGLU>
+[exitCode(2), cmdout] = system(cmdstrStashSubs, '-echo');
+[exitCode(3), cmdout] = system(cmdstrInit, '-echo');
+[exitCode(4), cmdout] = system(cmdstrPull, '-echo');
+
+if any(exitCode)
   error('gitUpdate:pull:pullFailed', 'Failed to pull latest changes:, %s', cmdout)
 end
 
-% Run any new tasks
-changesPath = fullfile(root, 'cortexlab', '+git', 'changes.m');
-if exist(changesPath, 'file')
-  git.changes;
-  delete(changesPath);
-end
-cd(origDir)
 end
