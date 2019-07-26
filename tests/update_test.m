56da25e85428fbd2fff6a9572607e838f8efb23c
jaib1 <j.bhagat@ucl.ac.uk>
2019-07-26 16:05:53 +0100

    created outline for git.update tests

---
+++ tests/update_test.m
@@ -0,0 +1,7 @@
+%% Test 1: input args tests: no input arg or wrong type
+
+%% Test 2: git tests: no FETCH_HEAD file for repo, git exe not found, or a system command fails
+
+%% Test 3: return from function tests: 3 different ways for not updating code
+
+%% Test 4: test proper functioning: if `scheduled` == 0 or `scheduled == weekday(today)`
