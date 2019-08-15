6a856feb827d5fb49421893602971b6dbbc0717b
k1o0 <k1o0@3tk.co>
2019-08-16 00:16:46 +0300

    Updates to docs

--- docs/setup/Burgess_setup.m
+++ docs/setup/Burgess_setup.m
@@ -1,45 +1,11 @@
-%% Configuring hardware devices 
-% When running <../../../+srv/expServer.m SRV.EXPSERVER> the hardware settings are loaded from a MAT
-% file and initialized before an experiment.  The MC computer may also have
-% a hardware file, though this isn't essential.  The below script is a
-% guide for setting up a new hardware file, with examples mostly pertaining
-% to replicating the Burgess steering wheel task(1).  Not all uncommented
-% lines will run without error, particularly when a specific hardware
-% configuration is required.  Always read the preceeding text before
-% running each line.
-%
-% It is recommended that you copy this file and keep it as a way of
-% versioning your hardware configurations.  In this way the script can
-% easily be re-run when after any unintended changes are made to your
-% hardware file.  If you do this, make sure you save a copy of the
-% calibration strutures or re-run the calibrations.
-%
-% Note that the variable names saved to the hardware file must be the same
-% as those below in order for various Rigbox functions to recognize them,
-% namely these variables:
-%
-% * |stimWindow| - The hw.Window object for PsychToolbox parameters
-% * |stimViewingModel| - A viewing model used by legacy experiments
-% * |mouseInput| - The rotary encoder device
-% * |lickDetector| - A lick detector device
-% * |timeline| - The Timeline object
-% * |daqController| - NI DAQ output settings for use during an experiment
-% * |scale| - A weighing scale device for use by the MC computer
-% * |screens| - Parameters for the Signals viewing model
-% * |audioDevices| - Struct of parameters for each audio device
-
-% Many of these classes for are found in the HW package:
-doc hw
+%% Burgess steering wheel
+% Below are some reasonable default hardware configurations for the Burgess
+% steering wheel task
 
 %% Retrieving hardware file path
 % The location of the configuration file is set in DAT.PATHS.  If running
 % this on the stimulus computer you can use the following syntax:
 hardware = fullfile(getOr(dat.paths, 'rigConfig'), 'hardware.mat');
-
-% For more info on setting the paths and using the DAT package:
-rigbox = getOr(dat.paths, 'rigbox'); % Location of Rigbox code
-open(fullfile(rigbox, 'docs', 'setup', 'paths_config.m'))
-open(fullfile(rigbox, 'docs', 'using_dat_package.m'))
 
 %% Configuring the stimulus window
 % The +hw Window class is the main class for configuring the visual
@@ -48,142 +14,41 @@ open(fullfile(rigbox, 'docs', 'using_dat_package.m'))
 % Currently the only concrete implementation is support for the
 % Psychophysics Toolbox, the HW.PTB.WINDOW class.
 
-doc hw.ptb.Window
 stimWindow = hw.ptb.Window;
 
-% Most of the properties directly mirror PsychToolbox parameters, therefore
-% it's recommended to check their documentation for clarification:
-%
-help Screen
-Screen OpenWindow? % Most properties are used as inputs to this function
-
-% Look at these for a deeper understanding of PTB:
-help PsychDemos
-help PsychBasic
-
-% Below are some of the more important properties:
-%
 %%% ScreenNum
 % The Windows screen index to display the stimulus on. If
 % Windows detects just one monitor (even if you have more plugged into the
 % graphics card), set this to 0 (meaning all screens). Otherwise if you
 % want just the primary display (the one with the menu bar), set it to 1;
 % secondary to 2, etc.
-stimWindow.ScreenNum = 0; % Use the single, main screen
+stimWindow.ScreenNum = 0; % Use all screen
 
 
-%%% SyncBounds
+%%% SyncBounds:
 % The area over which you can place a photodiode to record stimiulus update
-% times. A 4-element vector with [topLeftX topLeftY bottomRightX
-% bottomRightY] results in a square in that location that flips between the
-% values in SyncColourCycle each time the window updates (see 'Screen
-% Flip?').  By default the sync square alternates between black and white.
-% These filps can be acquired by Timeline in order to record the times at
-% which stimuli actually appeared on the monitor.  (See 'Timeline' section
-% below)
-
-% Leave this empty if you don't need to record the screen update times.  By
-% convention pixel [0, 0] is defined as the top left-most pixel of the
-% monitor. For a screen of 1024 px height create a 100 px^2 sync patch in
-% the bottom left corner of the screen:
-stimWindow.SyncBounds = [0 924 100 1024];
+% times. 
 % The simplist way to set this is with the POSITIONSYNCREGION method.
 % Let's put a 100 px^2 sync square in the top right of the window:
 stimWindow.positionSyncRegion('NorthEast', 100, 100)
 
 
-%%% SyncColourCycle 
-% A vector of luminance values or nx3 matrix RGB values to cycle through
-% each time the window updates.  Starts at the first index / row.
-% Cycle between black and white on each flip:
-stimWindow.SyncColourCycle = [0; 255];
-
-
 %%% PxDepth 
 % Sets the depth (in bits) of each pixel; default is 32 bits. You can
 % usually simply set it based on what the system uses:
-Screen PixelSize? % More info here
 stimWindow.PxDepth = Screen('PixelSize', stimWindow.ScreenNum);
 
 
 %%% OpenBounds 
 % The size and position of the window.  When left empty the screen will
-% cover the entire screen.  For debugging it is useful to set the bounds:
-res = Screen('Resolution', stimWindow.ScreenNum);
-% Set to 800x600 window 50 px from the top left:
-stimWindow.OpenBounds = [50,50,850,650]; 
-
-
-%%% DaqSyncEchoPort 
-% The DaqSyncEchoPort is the channel on which to output a pulse each time
-% the stimulus window is re-drawn.  This can be useful for convolving the
-% photodiode signal during analysis, particularly when the photodiode trace
-% is noisy.  It can also be a way of confirming whether a photodiode is
-% detecting all of the sync square changes.  Note: Sync pulses are not yet
-% supported in Signals, only in legacy experiments.
-
-% If this is left empty no sync pulse is set up.  
-% Ensure the DaqVendor and DaqDev properties are set correctly.  
-daq.getVendors % Query availiable vendors
-daq.getDevices % Query availiable devices and their IDs
-DaqSyncEchoPort = 'port1/line0'; % Output fulse on first digital output chan
-
-
-%%% BackgroundColour 
-% The clut index (scalar, [r g b] triplet or [r g b a] quadruple) defining
-% background colour of the stimulus window during legacy experiments.
-% These should be integers between 0-255.  If empty the default is usually
-% middle grey:
-stimWindow.BackgroundColour = 127*[1 1 1];
-
-% Note that for Signals experiment, the background colour can currently
-% only be at when calling SRV.EXPSERVER before an experiment, e.g.
-srv.expServer([], [0 127 127]) % Run the experiment with no red gun
-
+% cover the entire screen:
+stimWindow.OpenBounds = []; 
 
 %%% MonitorId 
 % A handy place to store the make or model of monitor used at that rig.  As
 % a copy of the hardware is saved each experiment this may be useful for
 % when looking back at old experiments in the future:
 stimWindow.MonitorId = 'LG LP097QX1'; % The screens used in Burgess et al.
-
-
-%%% PtbSyncTests 
-% A logical indicting whether or not to test synchronization to retrace
-% upon open.  When true it tests whether buffer flips are properly
-% synchronized to the vertical retrace signal of your display.  If these
-% tests fail PTB throws a warning but continues as normal. Synchronization
-% failiures indicate that there tareing or flickering may occur during
-% stimulus presentation.  More info on this may be found here:
-web('http://psychtoolbox.org/docs/SyncTrouble')
-% When blank the global setting is used:
-not(Screen('Preference', 'SkipSyncTests')) % Default true; run tests
-stimWindow.PtbSyncTests = true;
-
-
-%%% PtbVerbosity 
-% A number from 0 to 5 indicating the level of verbosity during the
-% experiment.  If empty the global preference is used.
-Screen('Preference', 'Verbosity') % Global verbosity setting
-% Below are the levels:
-% 0 - Disable all output - Same as using the SuppressAllWarnings flag.
-% 1 - Only output critical errors.
-% 2 - Output warnings as well.
-% 3 - Output startup information and a bit of additional information. This
-%     is the default.
-% 4 - Be pretty verbose about information and hints to optimize your code
-%     and system.
-% 5 - Levels 5 and higher enable very verbose debugging output, mostly
-%     useful for debugging PTB itself, not generally useful for end-users.
-stimWindow.PtbVerbosity = 2;
-
-
-%%% ColourRange, White, Black, etc. 
-% These properties are set by the object iteself after running OPEN, based
-% on the colour depth of the screen.  For more info see these docs:
-%
-help WhiteIndex
-help BlackIndex
 
 %% - Performing gamma calibration from command window
 %%% Calibration 
@@ -203,135 +68,6 @@ stimWindow.Calibration = stimWindow.calibration(DaqDev); % calibration
 
 save(hardware, 'stimWindow', '-append') % Save the stimWindow to file
 
-%% Using the Window object
-% Let's check the Window object is set up correctly and explore some of the
-% methods...
-
-%% - Setting the background colour
-stimWindow.open() % Open the window
-stimWindow.BackgroundColour = stimWindow.Green; % Change the background
-stimWindow.flip(); % Whoa!
-
-%% - Displaying a Gabor patch
-% Make a texture and draw it to the screen with MAKETEXTURE and DRAWTEXTURE
-% Let's make a Gabor patch as an example:
-sz = 1000; % size of texture matrix
-[xx, yy] = deal(linspace(-sz/2,sz/2,sz)');
-phi = 2*pi*rand; % randomised cosine phase
-sigma = 100; % size of Gaussian window
-thetaCos = 90; % grating orientation
-lambda = 100; % spatial frequency
-targetImg = vis.gabor(xx, yy, sigma, sigma, lambda, 0, thetaCos, phi);
-blankImg = repmat(stimWindow.Gray, [size(targetImg), 1]);
-targetImg = repmat(targetImg, [1 1 3]); % replicate three colour channels
-targetImg = round(blankImg.*(1 + targetImg));
-targetImg = min(max(targetImg, 0), 255); % Rescale values to 0-255
-
-% Convert the Gabor image to an OpenGL texture and load into buffer.
-% For more info: Screen MakeTexture?, Screen PreloadTextures?
-tex = stimWindow.makeTexture(round(targetImg));
-% Draw the texture into window (More info: Screen DrawTexture?)
-stimWindow.drawTexture(tex)
-% Flip the buffer:
-stimWindow.flip;
-
-%% - Clearing the window
-% To clear the window, the use CLEAR method:
-stimWindow.clear % Re-draw background colour
-stimWindow.flip; % Flip to screen
-
-%% - Drawing text to the screen
-% Drawing text to the screen can be done with the DRAWTEXT method:
-[x, y] = deal('center'); % Render the text to the center
-[nx, ny] = stimWindow.drawText('Hello World', x, y, stimWindow.Red);
-stimWindow.flip;
-
-% The nx and ny outputs may be used again as inputs to add to the text:
-[nx, ny] = stimWindow.drawText('Hello World', x, y, stimWindow.Red);
-stimWindow.drawText('! What''s up?', nx, ny, stimWindow.Red);
-stimWindow.flip;
-
-%% - Closing a window
-% Finally lets clear and close the window:
-stimWindow.clear
-stimWindow.close
-
-%% Viewing models
-% The viewing model classes allow one to configure the relationship between
-% physical dimentions and pixel space.  The viewing model classes contain
-% methods for converting between visual degrees, pixels and physical
-% dimentions.  Note: The viewing model classes are currently only
-% implemented in legacy experiments such as ChoiceWorld.  See below section
-% for configuring the viewing model in Signals.  
-%
-% There are currently two viewing model classes to choose from...
-
-%% - Basic screen viewing model
-% The basic viewing model class deals with single screens positioned
-% straight in front of an observer (^):| _____
-% |                                        ^  
-doc hw.BasicScreenViewingModel
-
-% Let's set this up:
-stimViewingModel = hw.BasicScreenViewingModel;
-% There are three parameters to set:
-% A position vector [x,y,z] of the subject in metres, with respect to
-% the (centre of the) top left pixel of the screen. x and y are aligned
-% with the standard graphics axes (i.e. x to the right, y going down),
-% while z extends out from the screen perpendicular to the plane of the
-% display).
-stimViewingModel.SubjectPos = [0, 0, 0.5]; % Observer centered at 50cm from screen
-
-% Number of pixels across the screen. Also see the function
-% USEGRAPHICSPIXELWIDTH to deduce this directly from the graphics hardware:
-stimViewingModel.useGraphicsPixelWidth(stimWindow.ScreenNum)
-stimViewingModel.ScreenWidthPixels % e.g. 1900 px
-
-% The physical width of the screen, in metres. Pixels are assumed to have a
-% 1:1 aspect ratio.
-stimViewingModel.ScreenWidthMetres = 0.4750; 
-
-save(hardware, 'stimViewingModel', '-append')
-
-%% -- Using the model
-% The object contains useful methods for converting between visual and
-% graphics space:
-
-% Visual field coordinates of a specified pixel.  The presumed
-% 'straight-ahead' view pixel should map to the centre of the visual field
-% (zero polar and visual angles)
-x = 0; y = 100; % Convert this pixel coordinate to visual angle
-[polarAngle, visualAngle] = stimViewingModel.viewAtPixel(x, y)
-% Polar angle is just the angle from central fixation pixel to specified
-% (and increases anticlockwise from horizon->right).
-
-% We can get the screen pixel of a given visual field locus. This may be
-% useful e.g. for placing stimuli at a certain point in the subject's visual
-% field. Let's convert
-% back to pixel space:
-[x, y] = stimViewingModel.pixelAtView(polarAngle, visualAngle) % ~[0, 100]
-
-% Visual angle between two pixel points.  This is useful if you want to
-% measure graphics dimensions in visual angles:
-[x2, y2] = deal(0); % Compare above to centre pixel
-rad = stimViewingModel.visualAngleBetweenPixels(x, y, x2, y2)
-% Radians to degrees:
-deg = rad2deg(rad)
-
-% Return the 'visual' pixel density (px per rad) at a point.  This is
-% useful for choosing spatial frequency of stimuli at a certain point on
-% the screen:
-pxPerRad = stimViewingModel.visualPixelDensity(x, y)
-% Screen distance in pixels, d, as a function of visual angle, t:
-% d(t) = zPx*tan(t)
-% Derivative w.r.t. t yields pixel density at a given visual angle:
-% d'(t) = zPx*sec(t)^2
-
-
-%% - Pseudo-Circular screen viewing model
-doc hw.PseudoCircularScreenViewingModel
-
-stimViewingModel = hw.PseudoCircularScreenViewingModel
 
 %% - Signals viewing model
 % Signals currently only supports a single viewing odel.  For now the
