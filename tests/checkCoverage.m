7200532ff9affbbb30f69a6ca056ffca19e93200
k1o0 <k1o0@3tk.co>
2019-08-16 11:01:38 +0300

    Added documentation for calibration test

--- tests/checkCoverage.m
+++ tests/checkCoverage.m
@@ -2,11 +2,18 @@ function results = checkCoverage(testFile, funLoc)
 % CHECKCOVERAGE Check the coverage of a given test
 %  Runs the tests for testFile with the coverage plugin.  Useful for seeing
 %  whether your test hits all lines of code.
+%
 %  Inputs:
 %    testFile (char) - file name of test
-%    funLoc (char) - folder location of function(s) being tested
+%    funLoc (char) - folder location of function(s) being tested (optional)
+%
 %  Output:
 %    results (array) - array of test result objects
+%
+%  Examples:
+%    checkCoverage('cellflat') % Folder can be inferred 
+%    checkCoverage('fun_package', fileparts(which('fun.run')))
+%
 
 narginchk(1,2)
 if nargin == 1
