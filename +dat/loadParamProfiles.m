89cc0e0e04115b77190e57471c0d7ce3a55dd557
Miles Wells <k1o0@3tk.co>
2019-12-17 13:50:33 +0200

    Support for alternate repos in loadParamProfiles

--- +dat/loadParamProfiles.m
+++ +dat/loadParamProfiles.m
@@ -6,20 +6,29 @@ function p = loadParamProfiles(expType)
 
 % 2013-07 CB created
 % 2017-02 MW Param struct now sorted in ASCII dictionary order
-
+narginchk(1,1)
 fn = 'parameterProfiles.mat';
-masterPath = fullfile(dat.reposPath('main', 'master'), fn);
+mainPath = fullfile(dat.reposPath('main', 'remote'), fn);
 
-p = struct; %default is to return an empty struct
+p = struct; % default is to return an empty struct
 
-if file.exists(masterPath)
+if any(file.exists(mainPath))
   origState = warning;
-  warning('off', 'MATLAB:load:variableNotFound'); %suppress not found warnings
-  loaded = load(masterPath, expType); %load profiles for specific experiment type
+  warning('off', 'MATLAB:load:variableNotFound'); % suppress not found warnings
+  if size(mainPath, 1) > 1
+      loaded = mapToCell(@(m)getOr(load(m, expType), expType, struct), mainPath); % load profiles for specific experiment type
+%       fnames = unique(cellflat(mapToCell(@fieldnames, loaded)));
+%       fvals = mapToCell(@(f)cell2mat(mapToCell(@(l)getOr(l,f), loaded)'), fnames);
+%       loaded = struct(expType, cell2struct(fvals,fnames))
+      merged = mergeStructs(loaded{:});
+      loaded = iff(isempty(fieldnames(merged)), merged, struct(expType, merged));
+  else
+      loaded = load(mainPath, expType);
+  end
   warning(origState);
   if isfield(loaded, expType)
     [~, I] = sort(lower(fieldnames(loaded.(expType))));
-    p = orderfields(loaded.(expType), I); %extract those profiles to return
+    p = orderfields(loaded.(expType), I); % extract those profiles to return
   end
 end
 
