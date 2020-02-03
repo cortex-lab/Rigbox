d6022c5771d539c706cb5aadc41fb92f6eafd16f
Miles Wells <k1o0@3tk.co>
2020-02-03 19:34:17 +0200

    Documented tabulateArgs

--- cb-tools/burgbox/tabulateArgs.m
+++ cb-tools/burgbox/tabulateArgs.m
@@ -1,14 +1,27 @@
 function [varargout] = tabulateArgs(varargin)
-%TABULATEARGS Turns a bunch of cell or single arguments into rows
+% TABULATEARGS Turns a bunch of cell or single arguments into rows
+%   [A1,...,AN,singleArg] = tabulateArgs(A1,...,AN) returns the mixed-size
+%   inputs as a cell array with any single element inputs replicated to be
+%   the same number of elements as the other inputs.  All non-single inputs
+%   must have the same number of elements.  Char arrays are considered to
+%   be a single element.  All output args thus have the same number of
+%   elements. If all inputs are single elements, they are returned
+%   unchanged (i.e. not as a cell).  Also returns a flag indicating whether
+%   all inputs were single elements.
+%
+%   Examples:
+%     [name, useFlag, singleArg] = tabulateArgs({'huxley', 'cajal'}, true)
+%     useFlag == {[1], [1]} % Now a cell array with equal size to `name`
+%     singleArg == false % numel(name) > 1 thus not all args were singles
+%
+%     [name, useFlag, singleArg] = tabulateArgs('huxley', true)
+%     useFlag == 1 % Unchanged
+%     singleArg == true % All inputs were single elements
 %
 % Part of Burgbox
 
 % 2013-03 CB created
 
-% Check if each argument is single, criteria are:
-% 1) *not* a cell array of *any* size, AND
-% 2) number of elements in whatever they are is 1, UNLESS
-% 3) it is a char, in which case it counts a single item (even if len > 1)
 singleArg = cellfun(@(arg) ~iscell(arg) && numel(arg) == 1 || ischar(arg), varargin);
 allSingleArgs = all(singleArg);
 
