10bcb5dc17efb58527efc81e6efd9051b45926cc
Miles Wells <k1o0@3tk.co>
2020-02-14 15:57:30 +0200

    Merge from master and updates to README, addRigboxPaths

--- +hw/+ptb/Window.m
+++ +hw/+ptb/Window.m
@@ -676,6 +676,8 @@ classdef Window < hw.Window
 
       % correct the data
       clock = circshift(clock,[ishift,0]);
+      upCrossings = find(diff( clock > 1 ) ==  1);
+      dnCrossings = find(diff( clock > 1 ) == -1);
       
       %% plot the data
       if makePlot
@@ -684,10 +686,7 @@ classdef Window < hw.Window
         
         figure; plot(tt,clock);
         ylabel('clock signal'); title('Clock');
-        
-        upCrossings = find(diff( clock > 1 ) ==  1);
-        dnCrossings = find(diff( clock > 1 ) == -1);
-        
+    
         figure; clf
         for iC = 1:length(upCrossings)
           plot(tt(upCrossings(iC))*[1 1],[0 5],'-', ...
