550ebda7001e08008ba7efff8db27717a8376eef
k1o0 <k1o0@3tk.co>
2020-02-14 15:21:06 +0200

    Documentation (#261)
    * Typo fix; added acknowledgement; mentioned 2PCs in software req
    
    * Generic paths template
    
    * added more steering wheel setup details

--- docs/setup/Burgess_setup.m
+++ docs/setup/Burgess_setup.m
@@ -1,6 +1,44 @@
-%% Burgess steering wheel
-% Below are some reasonable default hardware configurations for the Burgess
-% steering wheel task
+%% Burgess steering wheel task
+% Our laboratory developed a steering wheel setup to probe mouse
+% behavior(1). In this setup, a mouse turns a steering wheel with its front
+% paws to indicate whether a visual stimulus appears to its left or to its
+% right.
+%
+% This setup is being adopted in multiple laboratories, from Stanford to
+% Tokyo, and is being deployed by the International Brain Laboratory.
+%
+% To facilitate this deployment, we here provide instructions to build the
+% setup with components that are entirely off-the-shelf or 3-D printed
+% 
+% <<SteeringWheelBack.png>>
+% 
+
+%% Introduction
+% This document gives instructions on how to build a basic version of the
+% steering wheel setup to probe mouse behavior, introduced by Burgess et
+% al. The goal is to make it easy for other laboratories, including those
+% that make the International Brain Laboratory, to replicate the task and
+% extend it in various directions. To this end, these instructions rely
+% entirely on materials that can be bought off the shelf, or ordered online
+% based on 3-D drawings. In this steering wheel setup, we place a steering
+% wheel under the front paws of a head-fixed mouse, and we couple the
+% wheel's rotation to the horizontal position of a visual stimulus on the
+% screens. Turning the wheel left or right moves the stimulus left or
+% right. The mouse is then trained to decide whether a stimulus appears on
+% its left or its right. Using the wheel, the mouse indicates its choice by
+% moving the stimulus to the center. A correct decision is rewarded with a
+% drop of water and short intertrial interval, while an incorrect decision
+% is penalized with a longer timeout and auditory noise. We use this setup
+% throughout our laboratory, and deploy it in training rigs and
+% experimental rigs. Training rigs are used to train head-fixed mice on the
+% steering-wheel task and acquire behavioral data. Experimental rigs have
+% additional apparatus to collect electrophysiological and imaging data,
+% measure eye movements and licking activity, provide optogenetic
+% perturbations, and so on. Up until recently, constructing these setups
+% required a machine shop that could provide custom-made components.
+% However, for the purposes of spreading this setup to other laboratories,
+% we here describe a new version that does not require a machine shop: all
+% components can be ordered online or 3D-printed.
 
 %% Retrieving hardware file path
 % The location of the configuration file is set in DAT.PATHS.  If running
@@ -374,7 +412,7 @@ d = daq.getDevices % Availiable devices and their info
 % (1) <https://doi.org/10.1016/j.celrep.2017.08.047 DOI:10.1016/j.celrep.2017.08.047>
 
 %% Etc.
-% Author: Miles Wells
+% Authors: Lauren E Wool, Miles Wells, Hamish Forrest, and Matteo Carandini
 % v1.1.0
 
 %#ok<*NOPTS>
--- docs/setup/paths_config.m
+++ docs/setup/paths_config.m
@@ -35,7 +35,36 @@ source = fullfile(root, 'docs', 'setup', 'paths_template.m');
 destination = fullfile(root, '+dat', 'paths.m');
 assert(copyfile(source, destination), 'Failed to copy the template file')
 
+%% Sharing a folder in Windows
+% If you don’t yet have a data server, follow the steps below to set up a
+% shared folder on the stimulus server computer:
+%
+% # Create a folder in C:\ called ‘LocalExpCode’ and one called
+% ‘LocalExpData’ (if it doesn’t already exist)
+% # Copy everything inside the GitHub\Rigbox\Repositories\data folder into
+% ‘LocalExpData’ and everything inside GitHub\Rigbox\Repositories\code into
+% ‘LocalExpCode’
+% # Right click on the ‘LocalExpData’ folder and select Properties and
+% select the Sharing tab and click ‘Share...’
+% # Under the drop-down list select ‘Everyone’ and click ‘Add’, then
+% ‘Share’.
+% # Now click ‘Advanced Sharing…’, make sure the ‘Share this folder’ check
+% box is selected.  Click the ‘Permissions’ button and ensure that under
+% the ‘Permissions for Everyone’ section, the ‘Full Control’ is allowed.
+% # Repeat step 3, 4 and 5 for ‘LocalExpCode’.
+% # You should now be able to navigate to these folder from other computers
+% on the network be going to \\<StimulusServerName>\LocalExpData (where
+% ‘<StimulusServerName>’ is the computer name of the stimulus server)
+% # In dat.paths, change line 21 to be the following: serverName =
+% '\\<StimulusServerName>';  % where '<StimulusServerName>'    is the
+% stimulus server’s computer name
+% # On lines 26 and 32, replace ‘data’ with ‘LocalExpData’ and on lines 37
+% and 41, replace the word ‘code’ with ‘LocalExpCode’.
+% # Save the paths file in Documents\MATLAB\+dat and do the same on the mc
+% computer (the two computers must have the same paths).
+
+
 %% Etc.
 % Author: Miles Wells
 %
-% v0.0.1
+% v0.0.2
