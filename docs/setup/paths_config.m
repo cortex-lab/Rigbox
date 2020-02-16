8f6bbba2ba959a4423cf09d136859c4ce856e9aa
Jai Bhagat <j.bhagat@ucl.ac.uk>
2020-02-16 23:23:49 +0000

    updated some docs files to match dev

--- docs/setup/paths_config.m
+++ docs/setup/paths_config.m
@@ -20,17 +20,14 @@
 % determine where to save and load data.  
 %
 % Running the |addRigboxPaths| function should result in a copy of the
-% |paths_template| file being moved to the |+dat| folder. If running `which
+% paths template file being moved to the |+dat| folder. If running `which
 % dat.paths` shows this isn't the case, manually copy the template (see
 % below) and open the file.  The inline comments should explain each field.
 open dat.paths
 
 %% Manually copying the paths template
-% You can manually copy |docs\setup\paths_template.m| to Rigbox's |+dat|
-% folder using the Windows file explorer, or run the following lines in 
-% MATLAB to do so:
-
-% ensure Rigbox has been properly installed.
+% The below code should copy |docs\setup\paths_template.m| to
+% |+dat\paths.m|:
 assert(exist('addRigboxPaths','file') == 2, ...
   'Rigbox not installed.  Please run addRigboxPaths.m before continuing')
 root = fileparts(which('addRigboxPaths')); % Location of Rigbox root dir
@@ -38,36 +35,36 @@ source = fullfile(root, 'docs', 'setup', 'paths_template.m');
 destination = fullfile(root, '+dat', 'paths.m');
 assert(copyfile(source, destination), 'Failed to copy the template file')
 
+%% Sharing a folder in Windows
+% If you don’t yet have a data server, follow the steps below to set up a
+% shared folder on the stimulus server computer:
+%
+% # Create a folder in C:\ called ‘LocalExpCode’ and one called
+% ‘LocalExpData’ (if it doesn’t already exist)
+% # Copy everything inside the GitHub\Rigbox\Repositories\data folder into
+% ‘LocalExpData’ and everything inside GitHub\Rigbox\Repositories\code into
+% ‘LocalExpCode’
+% # Right click on the ‘LocalExpData’ folder and select Properties and
+% select the Sharing tab and click ‘Share...’
+% # Under the drop-down list select ‘Everyone’ and click ‘Add’, then
+% ‘Share’.
+% # Now click ‘Advanced Sharing…’, make sure the ‘Share this folder’ check
+% box is selected.  Click the ‘Permissions’ button and ensure that under
+% the ‘Permissions for Everyone’ section, the ‘Full Control’ is allowed.
+% # Repeat step 3, 4 and 5 for ‘LocalExpCode’.
+% # You should now be able to navigate to these folder from other computers
+% on the network be going to \\<StimulusServerName>\LocalExpData (where
+% ‘<StimulusServerName>’ is the computer name of the stimulus server)
+% # In dat.paths, change line 21 to be the following: serverName =
+% '\\<StimulusServerName>';  % where '<StimulusServerName>'    is the
+% stimulus server’s computer name
+% # On lines 26 and 32, replace ‘data’ with ‘LocalExpData’ and on lines 37
+% and 41, replace the word ‘code’ with ‘LocalExpCode’.
+% # Save the paths file in Documents\MATLAB\+dat and do the same on the mc
+% computer (the two computers must have the same paths).
+
+
 %% Etc.
 % Author: Miles Wells
 %
-% v0.0.1
-
-%% Setting up the paths
-% In order to use Rigbox, a 'paths' file must be placed in a |+dat| folder
-% somewhere in the MATLAB path. This file is a simple function that returns
-% a struct of paths to directories that Rigbox requires for running
-% experiments and managing data. You can copy |docs/setup/paths_template.m|
-% to |+dat/paths.m|, then customise the file according to your setup. The
-% paths used by the wider Rigbox code are found in the 'essential paths'
-% section of the |paths_template.m| file. These paths are required to run
-% experiments. Any number of custom repositories may be set, allowing them
-% to be queried using functions such as DAT.REPOSPATH and DAT.EXPPATH (see
-% below).
-%
-% It may be prefereable to keep the paths file in a shared network drive
-% where all rigs can access it.  This way only one file needs updating when
-% a path gets changed.  You can also override and add to the fields set by
-% the paths file in a rig specific manner.  To do this, create your paths
-% as a struct with the name `paths` and save this to a MAT file called
-% `paths` in your rig specific config folder:
-rigConfig = getOr(dat.paths('exampleRig'), 'rigConfig');
-customPathsFile = fullfile(rigConfig, 'paths.mat');
-paths.mainRepository = 'overide/path'; % Overide main repo for `exampleRig`
-paths.addedRepository = 'new/custom/path'; % Add novel repo
-
-save(customPathsFile, 'paths') % Save your new custom paths file.
-
-% More info in the paths template:
-root = getOr(dat.paths, 'rigbox');
-opentoline(fullfile(root, 'docs', 'setup', 'paths_template.m'), 75)
+% v0.0.2
