d9422030c4eb2d5211e5ece62baeaedfb189b96d
k1o0 <k1o0@3tk.co>
2020-04-16 23:52:44 +0300

    Removed range experiment code; added bgColour parameter support

--- +exp/configureSignalsExperiment.m
+++ +exp/configureSignalsExperiment.m
@@ -1,6 +1,29 @@
 function e = configureSignalsExperiment(paramStruct, rig)
-%UNTITLED13 Summary of this function goes here
-%   Detailed explanation goes here
+%exp.configureSignalsExperiment Setup Signals Experiment class
+%   Instantiate the exp.SignalsExp class and configure the object.
+%   Subclasses may be instantiated here using the type parameter.
+%
+%   Inputs:
+%     paramStruct : a SignalsExp parameter structure
+%     rig : a structure of rig hardware objects returned by hw.devices)
+%
+%   Output:
+%     e : a SignalsExp object
+%
+%   Example:
+%     rig = hw.devices;
+%     rig.stimWindow.open();
+%     pars = exp.inferParameters(@choiceWorld);
+%     e = exp.configureSignalsExperiment(pars, rig);
+%     e.run([]);
+%
+% See also exp.configureFilmExperiment, exp.configureChoiceExperiment
+
+%% Set the background colour
+% If the 'bgColour' parameter is defined, set the background colour
+if isfield(paramStruct, 'bgColour')
+  rig.stimWindow.BackgroundColour = paramStruct.bgColour;
+end
 
 %% Create the experiment object
 e = exp.SignalsExp(paramStruct, rig);
